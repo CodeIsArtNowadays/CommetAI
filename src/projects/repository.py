949eaf162e780bf64d70a8c1d1b167f1a56c9593
eraami's laptop <vrthurrain@gmail.com>
2026-04-30 20:34:36 +0300

    repo p1

--- src/projects/repository.py
+++ src/projects/repository.py
@@ -1,5 +1,6 @@
-from typing import List
+from typing import List, Sequence
 
+from sqlalchemy import select, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.projects.models import Project
@@ -12,16 +13,26 @@ class ProjectRepository:
         self.session = session
     
     async def create_project(self, project_data: ProjectCreateSchema) -> Project:
-        pass
+        project = Project(**project_data.model_dump())
+        self.session.add(project)
+        await self.session.commit()
+        await self.session.refresh(project)
+        return project
         
     async def get_project_by_id(self, id: int) -> Project:
-        pass
+        stmt = select(Project).where(Project.id==id)
+        res = await self.session.execute(stmt)
+        return res.scalar_one_or_none()
         
-    async def get_all_project_by_user(self, user_id: int) -> List[Project]:
-        pass
+    async def get_all_project_by_user(self, user_id: int) -> Sequence[Project]:
+        stmt = select(Project).where(Project.owner_id==user_id)
+        res = await self.session.execute(stmt)
+        return res.scalars().all()
         
     async def update_project(self, project_id: int, update_project: ProjectUpdateSchema) -> Project:
         pass
         
-    async def delete_project(self, project_id: int) -> Project:
-        pass
+    async def delete_project(self, project_id: int) -> None:
+        stmt = delete(Project).where(Project.id==id)
+        await self.session.execute(stmt)
+        
